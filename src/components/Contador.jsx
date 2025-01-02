2d4b91aee9c71c662ddf533081107b9367d2a8d9
ignaciojavier27 <nacholin124@gmail.com>
2025-01-02 18:42:21 -0300

    Agregando más funcionalidades al contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,14 +1,28 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 const initialState = { contador:0 };
+
+const TYPES = {
+    INCREMENTAR: "INCREMENTAR",
+    INCREMENTAR_5: "INCREMENTAR_5",
+    DECREMENTAR: "DECREMENTAR",
+    DECREMENTAR_5: "DECREMENTAR_5",
+    RESETEAR: "RESETEAR"
+}
 
 const reducer = ( state, action ) => {
 
     switch (action.type) {
-        case "INCREMENTAR":
+        case TYPES.INCREMENTAR:
             return { contador: state.contador + 1 }
-        case "DECREMENTAR":
+        case TYPES.DECREMENTAR:
             return { contador: state.contador - 1 }
+        case TYPES.DECREMENTAR_5:
+            return { contador: state.contador - action.payload }
+        case TYPES.INCREMENTAR_5:
+            return { contador: state.contador + action.payload }
+        case TYPES.RESETEAR:
+            return { contador: 0 }
         default:
             return state;
     }
@@ -23,18 +37,32 @@ const Contador = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     // const incrementar = () => setContador(contador + 1);
-    const incrementar = () => dispatch({type:"INCREMENTAR"});
+    const incrementar = () => dispatch({ type: TYPES.INCREMENTAR});
+
+    const incrementar5 = () => dispatch({ type: TYPES.INCREMENTAR_5, payload: 5 });
 
     // const decrementar = () => setContador(contador - 1);
-    const decrementar = () => dispatch({type:"DECREMENTAR"});
+    const decrementar = () => dispatch({type: TYPES.DECREMENTAR });
+
+    const decrementar5 = () => dispatch({ type: TYPES.DECREMENTAR_5, payload: 5});
+
+    const resetear = () => dispatch({ type: TYPES.RESETEAR });
+
 
 
     return (
     <div>
         <h2>Contador Reducer</h2>
+        <button onClick={decrementar5}>-5</button>
         <button onClick={decrementar}>-</button>
-        <span style={{margin:"0 10px"}}>{state.contador}</span>
+        <span 
+            style={{margin:"0 10px"}}
+            onClick={resetear}
+        >
+            {state.contador}
+        </span>
         <button onClick={incrementar}>+</button>
+        <button onClick={incrementar5}>+5</button>
     </div>
     )
 
